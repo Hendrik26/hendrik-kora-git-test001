23704840066c831a6080fb96c2ad654d1472c44d
hendrik_herdler <hendrik.herdler@korasoft.net>
2020-11-11 14:03:35 +0000

    Add AuthorsServiceHandler.java + corresponding (entity + service).

--- kora-books/srv/cat-service.cds
+++ kora-books/srv/cat-service.cds
@@ -1,5 +1,9 @@
 using my.bookshop as my from '../db/data-model';
 
+service AuthorsService {
+    @readonly entity Authors as projection on my.Authors;
+}
+
 service CatalogService {
     @readonly entity Books as projection on my.Books;
 }
---
+++ kora-books/srv/src/main/java/net/korasoft/books/handlers/AuthorsServiceHandler.java
@@ -0,0 +1,28 @@
+package net.korasoft.books.handlers;
+
+import java.util.stream.Stream;
+
+import org.springframework.stereotype.Component;
+
+import com.sap.cds.services.cds.CdsService;
+import com.sap.cds.services.handler.EventHandler;
+import com.sap.cds.services.handler.annotations.After;
+import com.sap.cds.services.handler.annotations.ServiceName;
+
+// import cds.gen.catalogservice.CatalogService_;
+// import cds.gen.ordersservice.OrdersService_;
+// import cds.gen.ordersservice.Books;
+// import cds.gen.catalogservice.Books;
+
+import cds.gen.authorsservice.AuthorsService_;
+import cds.gen.authorsservice.Authors;
+
+@Component
+@ServiceName(AuthorsService_.CDS_NAME) 
+public class AuthorsServiceHandler implements EventHandler {
+    @After(event = CdsService.EVENT_READ)
+    //@After(event = CdsService.EVENT_UPDATE)
+	public void doSomething(Stream<Authors> authors) {
+		int y = 3;
+	}
+}
